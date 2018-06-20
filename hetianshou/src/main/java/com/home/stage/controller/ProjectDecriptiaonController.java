a0042763571ba913967d4679fca2cdf480ea3bff
1215412143 <1215412143@qq.com>
2018-06-20 22:23:24 +0800

    index

--- hetianshou/src/main/java/com/home/stage/controller/ProjectDecriptiaonController.java
+++ hetianshou/src/main/java/com/home/stage/controller/ProjectDecriptiaonController.java
@@ -1,12 +1,15 @@
 package com.home.stage.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Administrator on 2018/6/2 0002.
  */
 public class ProjectDecriptiaonController {
-    public String index(HttpServletrequest httpservletrequest, HttpSevletResponse httpServletResponse){
+    public String index(HttpServletRequest httpservletrequest, HttpServletResponse httpServletResponse){
         return "views/stage/index";
     }
 }
